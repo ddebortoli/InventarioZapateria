3786c75b48a0a4e7bd86217d854e47a9b724cbf4
ddebortoli <damiandebortolilb@gmail.com>
2021-03-21 22:18:46 -0300

    Listo para master

--- indexOperations.py
+++ indexOperations.py
@@ -20,11 +20,11 @@ class mainOperations():
                 return result
 
 #Prueba para obtener registros
-#print(mainOperations('Prueba7','Prueba7',None,None,'OBTENER_ARTICULOS').callOperations())
+#print(mainOperations('Prueba7','Prueba7',None,None,'GET_ITEMS').callOperations())
 
 #Prueba para insertar un articulo en la bdd
-#print(mainOperations('Prueba8','Prueba7','A','1600','AGREGAR_ARTICULO').callOperations())
+#print(mainOperations('Prueba8','Prueba7','100','1600','ADD_ITEM').callOperations())
 
 #Prueba para eliminar un articulo, nombre es obligatorio
-#print(mainOperations('Prueba7','Prueba7','1000','1000','ELIMINAR_ARTICULOS').callOperations())
+#print(mainOperations('Prueba7','Prueba7','100','1000','DELETE_ITEM').callOperations())
 
